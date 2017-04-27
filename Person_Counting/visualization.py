188d251fe5c05f537be7bcb5dc3662c8b7280831
James Li <jamescfli@yahoo.com>
2017-04-27 17:56:45 +0800

    visualize both front and side on 20170419, moving side camera

--- Person_Counting/visualization.py
+++ Person_Counting/visualization.py
@@ -5,24 +5,35 @@ from matplotlib import dates
 import datetime
 from smoother import smooth
 
-id_count_0304_side = np.loadtxt('./outputs/id_accumulated_counter_20170304_side_SSD512x1024_sort_ma10_mh1.txt',
-                                dtype=np.int32, delimiter=',')
-id_count_0310_front = np.loadtxt('./outputs/id_accumulated_counter_20170310_front_SSD512x1024_sort_ma10_mh1.txt',
-                                 dtype=np.int32, delimiter=',')
+# id_count_0304_side = np.loadtxt('./outputs/id_accumulated_counter_20170304_side_SSD512x1024_sort_ma10_mh1.txt',
+#                                 dtype=np.int32, delimiter=',')    # sat
+# id_count_0310_front = np.loadtxt('./outputs/id_accumulated_counter_20170310_front_SSD512x1024_sort_ma10_mh1.txt',
+#                                  dtype=np.int32, delimiter=',')   # fri
+id_count_0419_front = np.loadtxt('./outputs/id_accumulated_counter_20170419_front_SSD512x1024_sort_ma10_mh3.txt',
+                                 dtype=np.int32, delimiter=',')      # wed
+id_count_0419_side = np.loadtxt('./outputs/id_accumulated_counter_20170419_side_SSD512x1024_sort_ma10_mh3.txt',
+                                dtype=np.int32, delimiter=',')      # wed
+
 
 # time_range = pd.timedelta_range("10:00:00", "21:59:00", freq="1min")
 
-id_count_0304_side_1dim = id_count_0304_side.ravel()
-id_count_0310_front_1dim = id_count_0310_front.ravel()
+# id_count_0304_side_1dim = id_count_0304_side.ravel()
+# id_count_0310_front_1dim = id_count_0310_front.ravel()
+id_count_0419_front_1dim = id_count_0419_front.ravel()
+id_count_0419_side_1dim = id_count_0419_side.ravel()
 
-id_count_per_min_0304_side = np.append(id_count_0304_side_1dim[0], np.diff(id_count_0304_side_1dim))
-id_count_per_min_0310_front = np.append(id_count_0310_front_1dim[0], np.diff(id_count_0310_front_1dim))
+# id_count_per_min_0304_side = np.append(id_count_0304_side_1dim[0], np.diff(id_count_0304_side_1dim))
+# id_count_per_min_0310_front = np.append(id_count_0310_front_1dim[0], np.diff(id_count_0310_front_1dim))
+id_count_per_min_0419_front = np.append(id_count_0419_front_1dim[0], np.diff(id_count_0419_front_1dim))
+id_count_per_min_0419_side = np.append(id_count_0419_side_1dim[0], np.diff(id_count_0419_side_1dim))
 
 # smoothing count per minute
 window_len_half = 5
 window_len = window_len_half*2 + 1
-id_count_per_min_0304_side_smoothed = smooth(id_count_per_min_0304_side, window_len=window_len, window='blackman')[window_len_half:-window_len_half]
-id_count_per_min_0310_front_smoothed = smooth(id_count_per_min_0310_front, window_len=window_len, window='blackman')[window_len_half:-window_len_half]
+# id_count_per_min_0304_side_smoothed = smooth(id_count_per_min_0304_side, window_len=window_len, window='blackman')[window_len_half:-window_len_half]
+# id_count_per_min_0310_front_smoothed = smooth(id_count_per_min_0310_front, window_len=window_len, window='blackman')[window_len_half:-window_len_half]
+id_count_per_min_0419_front_smoothed = smooth(id_count_per_min_0419_front, window_len=window_len, window='blackman')[window_len_half:-window_len_half]
+id_count_per_min_0419_side_smoothed = smooth(id_count_per_min_0419_side, window_len=window_len, window='blackman')[window_len_half:-window_len_half]
 
 # print id_count_per_min_0304_side_smoothed.shape
 # print id_count_per_min_0310_front_smoothed.shape
@@ -51,10 +62,13 @@ id_count_per_min_0310_front_smoothed = smooth(id_count_per_min_0310_front, windo
 mins = np.arange(720, dtype=np.int) + 10*60     # from 10 am
 times = np.array([datetime.datetime(2017, 3, 4, int(p/60), p%60) for p in mins])
 # and plot for every 5 samples:
-plt.plot(times, id_count_per_min_0310_front_smoothed, '-b')     # one value per 5 min
-plt.plot(times, id_count_per_min_0304_side_smoothed, '-.r')
-plt.legend(['20170310 Fri. Front', '20170304 Sat. Side'], loc='best')
-plt.title('Count per Minute')
+# plt.plot(times, id_count_per_min_0310_front_smoothed, '-b')     # one value per 5 min
+# plt.plot(times, id_count_per_min_0304_side_smoothed, '-.r')
+# plt.legend(['20170310 Fri. Front', '20170304 Sat. Side'], loc='best')
+plt.plot(times, id_count_per_min_0419_front_smoothed, '-b')
+plt.plot(times, id_count_per_min_0419_side_smoothed, '-.r')
+plt.legend(['Front', '20170419 Wed. Side'], loc='best')
+plt.title('Count per Minute on 20170419 Wed.')
 
 # generate a formatter, using the fields required
 fmtr = dates.DateFormatter("%H:%M")
